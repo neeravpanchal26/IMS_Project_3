8805a6e70f55396a8ec9bfe8c5fcffaf058766cd
Neerav Panchal <masterpanchieneerav@gmail.com>
2018-06-19 18:07:41 +0200

    Bootstrap implemented/Login page frontend done/ Header page started

--- IMS/src/app/app.module.ts
+++ IMS/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
---
+++ IMS/src/app/components/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  // Global Declaration
+  private iT = [ {'path': '/Add User', 'name': 'Add User'}, {'path': '/Remove User', 'name': 'Remove User'}];
+  private tE = [ {'path': '/Add Equipment', 'name': 'Add Equipment'}, {'path': '/Update Equipment', 'name': 'Update Equipment'},
+      {'path': '/Locate Equipment', 'name': 'Locate Equipment'}];
+  private sH = [ {'path': '/Void Equipment', 'name': 'Dispose Equipment'}];
+  private router = [];
+  private userType = null;
+  // Default Constructor
+  constructor() { }
+
+  // Page Load
+  ngOnInit() {
+    this.userType = 1;
+    if (this.userType === 1) {
+      this.router = this.iT;
+    } else if (this.userType === 2) {
+      this.router = this.tE;
+    } else if (this.userType === 3) {
+      this.router = this.tE.concat(this.sH);
+    }
+  }
+
+}
