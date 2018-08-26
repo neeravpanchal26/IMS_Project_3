7bf0f186dc75a4f48c1b468e01cdcd27b607900b
mlangeveld15 <36929407+mlangeveld15@users.noreply.github.com>
2018-08-26 12:47:20 +0200

    Leaftlet
    Marker draggable working as well as getting the marker's location on the spot as well

--- IMS/src/app/components/install-equipment/install-equipment.component.ts
+++ IMS/src/app/components/install-equipment/install-equipment.component.ts
@@ -20,19 +20,53 @@ export class InstallEquipmentComponent implements OnInit {
   public coords: any;
   public map: any;
   public equipment: any;
+  public marker: any;
   ngOnInit() {
+    let e: any;
+    console.log()
 
     this.geo.getLocation().subscribe(data => {
+      console.log(data);
       this.lat = data.coords.latitude;
       this.long = data.coords.longitude;
       console.log(this.lat + '  ' + this.long);
       this.map = L.map('mapid').setView([this.lat, this.long], 14);
-      this.IEService.loadMap(this.map, this.lat, this.long);
+      this.loadMap(this.map, this.lat, this.long);
     });
-    
-
-
     this.IEService.getInstallEquipment(this.login.getUserID()).subscribe(data => { this.equipment = data });
+  }
+
+  loadMap(mymap, lat, long) {
+    this.lat = lat;
+    this.long = long;
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
+      {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibWxhbmdldmVsZDE1IiwiYSI6ImNqa2R1a3A5aTFpeXkza252cDlkbDQ2MXIifQ.uTJ2w8n_rjeHtl1usY3K9Q'
+      }).addTo(mymap);
+    this.marker = L.marker([lat, long], {
+      icon: L.icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: '../assets/marker-icon.png',
+        shadowUrl: '../assets/marker-shadow.png'
+      }), draggable: true
+    }).openTooltip().addTo(mymap);
+    console.log(this.marker._latlng)
+    this.markerChange(this.marker);
+  }
+  markerChange(marker:any) {
+    console.log(marker);
+    marker.on('dragend',function(e:any)
+  {
+    let marker=e.target
+    let location = marker.getLatLng();
+    this.lat = location.lat;
+    this.lng = location.lng;
+    console.log(this.lat+', '+this.lng);
+  })
   }
 }
 
--- IMS/src/app/components/install-equipment/install-equipment.service.ts
+++ IMS/src/app/components/install-equipment/install-equipment.service.ts
@@ -1,39 +1,20 @@
-import { Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import * as L from 'leaflet';
 import 'leaflet/dist/images/marker-icon.png';
 
 @Injectable()
 export class InstallEquipmentService {
 
-     // Global variable
-  apiUrl = environment.api;
-  public marker: any;
-  // Default Constructor
-  constructor(private http:HttpClient) { }
+    // Global variable
+    apiUrl = environment.api;
+    public lat: any
+    public long: any;
+    // Default Constructor
+    constructor(private http: HttpClient) { }
 
-  getInstallEquipment(userID):Observable<any>
-  {
-      return this.http.get(this.apiUrl+'/api/BLL/installEquipment.php?action=install&userID='+userID)as Observable<any>;
-  }
-  loadMap(mymap,lat,long)
-  {
-      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-      {
-          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-          maxZoom: 18,
-          id: 'mapbox.streets',
-          accessToken: 'pk.eyJ1IjoibWxhbmdldmVsZDE1IiwiYSI6ImNqa2R1a3A5aTFpeXkza252cDlkbDQ2MXIifQ.uTJ2w8n_rjeHtl1usY3K9Q'
-        }).addTo(mymap);
-        this.marker = L.marker([lat, long],{
-            icon: L.icon({
-                iconSize: [ 25, 41 ],
-                iconAnchor: [ 13, 41 ],
-                iconUrl: '../assets/marker-icon.png',
-                shadowUrl: '../assets/marker-shadow.png'
-            })
-        }).openTooltip().addTo(mymap);
+    getInstallEquipment(userID): Observable<any> {
+        return this.http.get(this.apiUrl + '/api/BLL/installEquipment.php?action=install&userID=' + userID) as Observable<any>;
     }
 }
