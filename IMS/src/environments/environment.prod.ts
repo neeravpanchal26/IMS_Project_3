431d9adc578122bb8b104d4dbf60a6a00cb52eb2
Neerav Panchal <masterpanchieneerav@gmail.com>
2018-09-27 10:44:41 +0200

    27-09
    Build updates

--- IMS/src/environments/environment.prod.ts
+++ IMS/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-    api:'https://bit1ims.club'
+    // Digital Ocean
+    // api:'https://bit1ims.club'
+    // Nmu
+    api:'http://sict-iis.nmmu.ac.za'
+    // Local Setup
+    // api:'http://localhost:3000'
 };
